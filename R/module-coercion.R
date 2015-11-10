002d9b9439a8121f11ad866e8b68cf1a5dc724cf
Sebastian Warnholz <wahani@gmail.com>
2015-11-10 15:28:37 +0100

    added as.module

---
+++ R/module-coercion.R
@@ -0,0 +1,13 @@
+#' Coercion for Modules
+#'
+#' Interfaces to and from modules.
+#'
+#' @param filename (character or connection) a file to be sourced as module
+#' @param ... arguments passed to \link{parse}
+#' @inheritParams module
+#'
+#' @export
+#' @rdname modulecoerce
+as.module <- function(filename, parent = baseenv(), ...) {
+  do.call(module, list(parse(filename, ...), parent))
+}
