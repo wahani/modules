8b612e5929ccb82f4632386488f7fe7752ffca9b
Sebastian <wahani@gmail.com>
2016-01-30 11:53:36 +0100

    fix for batches in vignette / readme

--- prepareRepo.R
+++ prepareRepo.R
@@ -1,2 +1,15 @@
 devtools::build_vignettes()
 knitr::knit("inst/doc/modulesInR.Rmd", "README.md")
+
+text <- c(
+  "[![Build Status](https://travis-ci.org/wahani/modules.png?branch=master)](https://travis-ci.org/wahani/modules)",
+  "[![codecov.io](https://codecov.io/github/wahani/modules/coverage.svg?branch=master)](https://codecov.io/github/wahani/modules?branch=master)",
+  "[![CRAN](http://www.r-pkg.org/badges/version/modules)](https://cran.r-project.org/package=modules)",
+  "![Downloads](http://cranlogs.r-pkg.org/badges/modules)",
+  readLines(
+    "README.md"
+  )
+)
+
+writeLines(text, "README.md")
+
