9f59ea3b8660b23c09c8e747c1e2a3c629208adb
Sebastian Warnholz <wahani@users.noreply.github.com>
2023-06-10 10:55:47 +0200

    Allow to export names containing special characters (#44)
    resolves #43

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -1,8 +1,19 @@
+test_that("Exports of special names #43", {
+  m <- module({
+    "==.foo" <- function(lhs, rhs) base::`==`(lhs, rhs) # Exclude Linting
+    "!.foo" <- function(lhs, rhs) base::`!=`(lhs, rhs)  # Exclude Linting
+  })
+  testthat::expect_true(m$`==.foo`(1, 1))
+  testthat::expect_true(m$`!.foo`(1, 2))
+})
+
 test_that("Exports of special names #37", {
   m <- module({
     "%+%" <- function(lhs, rhs) lhs + rhs # Exclude Linting
+    "%add%" <- `%+%`
   })
   testthat::expect_true(m$`%+%`(1, 2) == 3)
+  testthat::expect_true(m$`%add%`(1, 2) == 3)
 })
 
 test_that("Exports of names with whitespace #39", {
